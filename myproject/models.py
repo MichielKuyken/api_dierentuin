470141fd6cb7149275783276e10f9a771240da57
MichielKuyken <r0938842@student.thomasmore.be>
2023-12-29 16:57:19 +0100

    code upload

--- myproject/models.py
+++ myproject/models.py
@@ -12,16 +12,14 @@ class Manager(Base):
     achternaam = Column(String)
     manager_nummer = Column(String, unique=True, index=True)
 
-    regios = relationship("Regio", back_populates="managers", foreign_keys="[Regio.manager_id]")
+    dieren = relationship("Dier", back_populates="managers", foreign_keys="[Dier.manager_id]")
 
 
 class Regio(Base):
     __tablename__ = "regios"
     id = Column(Integer, primary_key=True, index=True)
     regionaam = Column(String, unique=True)
-    manager_id = Column(Integer, ForeignKey("managers.id"))
 
-    managers = relationship("Manager", back_populates="regios", foreign_keys="[Regio.manager_id]")
     dieren = relationship("Dier", back_populates="regios", foreign_keys="[Dier.regio_id]")
 
 
@@ -31,8 +29,10 @@ class Dier(Base):
     diersoort = Column(String, unique=True)
     hoeveelheid = Column(Integer)
     regio_id = Column(Integer, ForeignKey("regios.id"))
+    manager_id = Column(Integer, ForeignKey("managers.id"))
 
     regios = relationship("Regio", back_populates="dieren", foreign_keys="[Dier.regio_id]")
+    managers = relationship("Manager", back_populates="dieren", foreign_keys="[Dier.manager_id]")
 
 
 class Eigenaar(Base):
